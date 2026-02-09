41ea9498e662b597438bf3cfb6ca409f2a419a4a
e04 <47185462+e04@users.noreply.github.com>
2026-02-09 23:06:47 +0900

    add dummy gui_export

--- gcoordinator/__init__.py
+++ gcoordinator/__init__.py
@@ -3,3 +3,5 @@ from gcoordinator.path_transformer import Transform
 from gcoordinator.infill_generator import gyroid_infill, line_infill
 from gcoordinator.gcode_generator  import GCode
 from gcoordinator.settings         import set_settings, get_settings, template_settings
+from gcoordinator.gui_export       import gui_export
+
---
+++ gcoordinator/gui_export.py
@@ -0,0 +1,2 @@
+def gui_export(full_object):
+    print("gui_export is not supported. Instead, store it in a global variable named `full_object`.")
